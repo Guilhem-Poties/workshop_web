a424c52ec625b6271fc1d8bd191b671150e6022a
Guilhem <gpoties@gmail.com>
2024-06-26 11:31:44 +0200

    Nouvelle version de la bd avec auto incrément

--- quizoo.sql
+++ quizoo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : mar. 25 juin 2024 à 14:04
--- Version du serveur :  5.7.34
--- Version de PHP : 7.4.21
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 26 juin 2024 à 09:31
+-- Version du serveur : 8.0.37
+-- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,21 +27,16 @@ SET time_zone = "+00:00";
 -- Structure de la table `question`
 --
 
-CREATE TABLE `question` (
-  `id_question` int(11) NOT NULL,
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` int NOT NULL,
   `libelle` varchar(50) NOT NULL,
-  `id_theme` int(11) NOT NULL,
-  `id_bonne_reponse` int(11) DEFAULT NULL
+  `id_theme` int NOT NULL,
+  `id_bonne_reponse` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_theme` (`id_theme`),
+  UNIQUE KEY `id_bonne_reponse` (`id_bonne_reponse`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Déchargement des données de la table `question`
---
-
-INSERT INTO `question` (`id_question`, `libelle`, `id_theme`, `id_bonne_reponse`) VALUES
-(0, 'Que produisent les vaches ?', 0, 0),
-(1, 'Les moutons sont ?', 0, 4),
-(2, 'Comment s’appelle le petit de la poule ?', 0, 7);
 
 -- --------------------------------------------------------
 
@@ -49,27 +44,15 @@ INSERT INTO `question` (`id_question`, `libelle`, `id_theme`, `id_bonne_reponse`
 -- Structure de la table `reponse`
 --
 
-CREATE TABLE `reponse` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `reponse`;
+CREATE TABLE IF NOT EXISTS `reponse` (
+  `id` int NOT NULL,
   `libelle` varchar(50) NOT NULL,
   `image` varchar(100) DEFAULT NULL,
-  `id_question` int(11) NOT NULL
+  `id_question` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_question` (`id_question`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Déchargement des données de la table `reponse`
---
-
-INSERT INTO `reponse` (`id`, `libelle`, `image`, `id_question`) VALUES
-(0, 'Du lait', NULL, 0),
-(1, 'Du fromage', NULL, 0),
-(2, 'Des oeufs', NULL, 0),
-(3, 'Carnivore', NULL, 1),
-(4, 'Herbivore', NULL, 1),
-(5, 'Omnivore', NULL, 1),
-(6, 'Le veau', NULL, 2),
-(7, 'Le poussin', NULL, 2),
-(8, 'Le coq', NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -77,12 +60,16 @@ INSERT INTO `reponse` (`id`, `libelle`, `image`, `id_question`) VALUES
 -- Structure de la table `session`
 --
 
-CREATE TABLE `session` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE IF NOT EXISTS `session` (
+  `id` int NOT NULL,
   `date` date NOT NULL,
-  `score` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_theme` int(11) NOT NULL
+  `score` int NOT NULL,
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `id_theme` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_user` (`id_user`),
+  UNIQUE KEY `id_theme` (`id_theme`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
 
 -- --------------------------------------------------------
@@ -91,18 +78,13 @@ CREATE TABLE `session` (
 -- Structure de la table `theme`
 --
 
-CREATE TABLE `theme` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `theme`;
+CREATE TABLE IF NOT EXISTS `theme` (
+  `id` int NOT NULL,
   `libelle` varchar(50) NOT NULL,
-  `description` varchar(500) NOT NULL
+  `description` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
---
--- Déchargement des données de la table `theme`
---
-
-INSERT INTO `theme` (`id`, `libelle`, `description`) VALUES
-(0, 'Les animaux de la ferme', 'Devient incollable sur la vache le coq et leurs amis!');
 
 -- --------------------------------------------------------
 
@@ -110,53 +92,23 @@ INSERT INTO `theme` (`id`, `libelle`, `description`) VALUES
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   `mail` varchar(50) NOT NULL,
   `date_naissance` date NOT NULL,
-  `mdp` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+  `mdp` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf16;
 
 --
--- Index pour les tables déchargées
+-- Déchargement des données de la table `utilisateur`
 --
 
---
--- Index pour la table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`id_question`),
-  ADD UNIQUE KEY `id_bonne_reponse` (`id_bonne_reponse`),
-  ADD KEY `id_theme` (`id_theme`) USING BTREE;
-
---
--- Index pour la table `reponse`
---
-ALTER TABLE `reponse`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_question` (`id_question`) USING BTREE;
-
---
--- Index pour la table `session`
---
-ALTER TABLE `session`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_user` (`id_user`),
-  ADD UNIQUE KEY `id_theme` (`id_theme`);
-
---
--- Index pour la table `theme`
---
-ALTER TABLE `theme`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `mail`, `date_naissance`, `mdp`) VALUES
+(1, 'sou', 'ma', 'souma@gashemene.nor', '2024-06-11', 'mdp');
 
 --
 -- Contraintes pour les tables déchargées
@@ -166,21 +118,21 @@ ALTER TABLE `utilisateur`
 -- Contraintes pour la table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`id_bonne_reponse`) REFERENCES `reponse` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Contraintes pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `session`
 --
 ALTER TABLE `session`
-  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `session_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `utilisateur` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `session_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
