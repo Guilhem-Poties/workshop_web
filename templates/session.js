5fe732a3cca84f7a963d037f98f5936187ea4047
Guilhem <gpoties@gmail.com>
2024-06-28 13:56:07 +0200

    Correction du bug au premier affichage du questionnaire

--- templates/session.js
+++
@@ -1,6 +0,0 @@
-function setUser(id) {
-    window.localStorage.setItem("user") = id;
-}
-async function getUser() {
-    const user = await fetch("/", {method: "POST", body: window.localStorage.getItem("user")});
-}
